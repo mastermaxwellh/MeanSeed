b16563285d8cfc76a29ff91d6bc24c790efacb48
Max Heath <maxheaththetcgplayer@gmail.com>
2018-01-21 09:59:22 -0500

    attempted bugfixing

--- app/routes.js
+++ app/routes.js
@@ -29,7 +29,7 @@ var Nerd = require('../models/nerd');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('*', function(req, res) {
-            res.sendFile('./public/views/index.html'); // load our public/index.html file
+            res.sendFile('../public/views/index.html'); // load our public/index.html file
         });
 
     };
