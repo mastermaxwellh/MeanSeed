0c0e5b4d91636094cad6a91f8c2c68808ff0f061
Max Heath <maxheaththetcgplayer@gmail.com>
2018-01-28 11:24:04 -0500

    update

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
+var MongoClient = require("mongodb").MongoClient;
+var assert = require("assert");
 
 // configuration ===========================================
 
@@ -17,7 +19,30 @@ var port = process.env.PORT || 8080;
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-mongoose.connect(db.url); 
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+}
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    const db = client.db(dbName);
+  
+    insertDocuments(db, function() {
+      client.close();
+    });
+});
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
